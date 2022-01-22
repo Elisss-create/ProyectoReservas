88ba4f85a8ed978f62a5792ae188142ad850be0e
Eliana Soria <meliana_16@hotmail.com>
2022-01-21 22:17:54 -0300

    simprificando funcion td

--- js/js.js
+++ js/js.js
@@ -3,44 +3,16 @@ var botonReservar = document.querySelector("#botonReservar");
 botonReservar.addEventListener("click", function (event) {
     event.preventDefault();
 
-    var form= document.querySelector("#formReserva");
+    var form = document.querySelector("#formReserva");
 
     var usuario = capturarDatosUsuario(form);
-    console.log(usuario);
+    var usuarioTr = crearUsuario(usuario);
 
-    var tabla = document.querySelector("#tablaClientes");
-
-    //creamos td y tr de los campos de elementos
-    clienteTr = document.createElement("tr");
-    nombreTd = document.createElement("td");
-    fechaTd = document.createElement("td");
-    horaDesdeTd = document.createElement("td");
-    horaHastaTd = document.createElement("td");
-    salaTd = document.createElement("td");
-
-    //Asigno los valores a la propiedad textContent 
-    nombreTd.textContent = usuario.nombre;
-    fechaTd.textContent = usuario.fecha;
-    horaDesdeTd.textContent = usuario.horaD;
-    horaHastaTd.textContent = usuario.horaH;
-    salaTd.textContent = usuario.sala;
-
-
-    if (usuario.sala === "Seleccionar" || usuario.nombre === "") {
-        alert("Complete los campos");
-
-    } else {
-        //asignacion de los tr y td a la tabla
-        tabla.appendChild(clienteTr);
-        clienteTr.appendChild(nombreTd);
-        clienteTr.appendChild(fechaTd);
-        clienteTr.appendChild(horaDesdeTd)
-        clienteTr.appendChild(horaHastaTd);
-        clienteTr.appendChild(salaTd);
-    }
-
+    var tabla = document.querySelector("#tablaUsuario");
+    tabla.appendChild(usuarioTr);
+    form.reset();
+    
 });
-
 
 function capturarDatosUsuario(form) {
     var usuario = {
@@ -51,4 +23,33 @@ function capturarDatosUsuario(form) {
          sala :form.sala.value
     }
     return usuario;
+}
+
+function crearUsuario(usuario){
+
+//creamos td y tr de los campos de elementos
+var usuarioTr = document.createElement("tr");
+usuarioTr.classList.add("usuario");
+
+    if (usuario.sala === "Seleccionar" || usuario.nombre === "") {
+        alert("Complete los campos");
+
+    } else {
+        //asignacion de los tr y td a la tabla
+
+        usuarioTr.appendChild(construirTd(usuario.nombre,"info-nombre"));
+        usuarioTr.appendChild(construirTd(usuario.fecha,"info-fecha"));
+        usuarioTr.appendChild(construirTd(usuario.horaD,"info-desde"));
+        usuarioTr.appendChild(construirTd(usuario.horaH,"info-hasta"));
+        usuarioTr.appendChild(construirTd(usuario.sala,"info-sala"));
+
+    } 
+    return usuarioTr;
+}
+
+function construirTd(dato, clase) {
+    var td = document.createElement("td");
+    td.classList.add(clase);
+    td.textContent = dato;
+    return td;
 }
